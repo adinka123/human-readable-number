b7fbd11ade050e4cfda3a870739af51554fcb2e6
Adina Umbetova <adinaumbetova@gmail.com>
2023-02-04 02:01:00 +0600

    fix:delete wrong solutions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 
 module.exports = function toReadable (number) {
-  const numbers = ["zero","one","two","three","four","five","six","seven","eight","nine","ten",
-                     "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred"];
-    let output = "";
-    if (number < 0 || number > 1000) {
-        return 'out of bound';
-    } else {
-        if (number >= 100) {
-            output += numbers[Math.floor(number/100)] + " hundred ";
-            number %= 100;
-        }
-        if (number >= 20) {
-            output += numbers[Math.floor(number/10)+18] + " ";
-            number %= 10;
-        }
-        if (number > 0 && number < 20) {
-            output += numbers[number] + " ";
-        }
-        return output;
-      }
+ 
 }
